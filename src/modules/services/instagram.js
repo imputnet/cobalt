aed92f999a351fa459076d84c1ef3927398f8640
radkii <real.radkii@gmail.com>
2022-08-02 15:57:28 +0100

    Update instagram.js

--- src/modules/services/instagram.js
+++ src/modules/services/instagram.js
@@ -6,35 +6,6 @@ import { genericUserAgent, maxVideoDuration } from "../config.js";
 
 let login = false;
 
-var fs = require('fs');
-function move(oldPath, newPath, callback) {
-    fs.rename(oldPath, newPath, function (err) {
-        if (err) {
-            if (err.code === 'EXDEV') {
-                copy();
-            } else {
-                callback(err);
-            }
-            return;
-        }
-        callback();
-    });
-
-    function copy() {
-        var readStream = fs.createReadStream(oldPath);
-        var writeStream = fs.createWriteStream(newPath);
-
-        readStream.on('error', callback);
-        writeStream.on('error', callback);
-
-        readStream.on('close', function () {
-            fs.unlink(oldPath, callback);
-        });
-
-        readStream.pipe(writeStream);
-    }
-}
-
 export default async function(obj) {
     try {
         if(!login){
