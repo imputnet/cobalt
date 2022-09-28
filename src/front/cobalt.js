6972be257c565840bcd58ea695fdb81171b08f0e
wukko <me@wukko.me>
2022-09-28 18:34:44 +0600

    forgot to change the version in front js

--- src/front/cobalt.js
+++ src/front/cobalt.js
@@ -1,5 +1,5 @@
 let isIOS = navigator.userAgent.toLowerCase().match("iphone os");
-let version = 10;
+let version = 11;
 let regex = new RegExp(/https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/);
 
 let switchers = {
