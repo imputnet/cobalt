1a1a4534b73138fc6b7a466fbcd7863ceadfac0f
wukko <me@wukko.me>
2022-12-21 10:46:36 +0600

    fixed an oopsie in tumblr module

--- src/modules/services/tumblr.js
+++ src/modules/services/tumblr.js
@@ -8,7 +8,7 @@ export default async function(obj) {
         }).then((r) => {return r.text()}).catch(() => {return false});
         if (!html) return { error: 'ErrorCouldntFetch' };
         if (html.includes('property="og:video" content="https://va.media.tumblr.com/')) {
-            return { urls: `https://va.media.tumblr.com/${html.split('property="og:video" content="https://va.media.tumblr.com/')[1].split('"/>')[0]}`, audioFilename: `tumblr_${obj.id}_audio` }
+            return { urls: `https://va.media.tumblr.com/${html.split('property="og:video" content="https://va.media.tumblr.com/')[1].split('"')[0]}`, audioFilename: `tumblr_${obj.id}_audio` }
         } else return { error: 'ErrorEmptyDownload' }
     } catch (e) {
         return { error: 'ErrorBadFetch' };
