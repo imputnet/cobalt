4a36e638cf242ec34da592e821cf3ca9a8e2ddf9
wukko <me@wukko.me>
2023-01-18 17:27:57 +0600

    catching even more events to kill ffmpeg

--- src/modules/stream/types.js
+++ src/modules/stream/types.js
@@ -48,7 +48,9 @@ export function streamLiveRender(streamInfo, res) {
             res.setHeader('Connection', 'keep-alive');
             res.setHeader('Content-Disposition', `attachment; filename="${streamInfo.filename}"`);
             ffmpegProcess.stdio[3].pipe(res);
-            ffmpegProcess.on('end', () => ffmpegProcess.kill());
+            ffmpegProcess.on('disconnect', () => ffmpegProcess.kill());
+            ffmpegProcess.on('close', () => ffmpegProcess.kill());
+            ffmpegProcess.on('exit', () => ffmpegProcess.kill());
             res.on('finish', () => ffmpegProcess.kill());
             res.on('close', () => ffmpegProcess.kill());
             ffmpegProcess.on('error', (err) => {
@@ -91,7 +93,9 @@ export function streamAudioOnly(streamInfo, res) {
         res.setHeader('Connection', 'keep-alive');
         res.setHeader('Content-Disposition', `attachment; filename="${streamInfo.filename}.${streamInfo.audioFormat}"`);
         ffmpegProcess.stdio[3].pipe(res);
-        ffmpegProcess.on('end', () => ffmpegProcess.kill());
+        ffmpegProcess.on('disconnect', () => ffmpegProcess.kill());
+        ffmpegProcess.on('close', () => ffmpegProcess.kill());
+        ffmpegProcess.on('exit', () => ffmpegProcess.kill());
         res.on('finish', () => ffmpegProcess.kill());
         res.on('close', () => ffmpegProcess.kill());
         ffmpegProcess.on('error', (err) => {
@@ -121,7 +125,9 @@ export function streamVideoOnly(streamInfo, res) {
         res.setHeader('Connection', 'keep-alive');
         res.setHeader('Content-Disposition', `attachment; filename="${streamInfo.filename.split('.')[0]}_mute.${format}"`);
         ffmpegProcess.stdio[3].pipe(res);
-        ffmpegProcess.on('end', () => ffmpegProcess.kill());
+        ffmpegProcess.on('disconnect', () => ffmpegProcess.kill());
+        ffmpegProcess.on('close', () => ffmpegProcess.kill());
+        ffmpegProcess.on('exit', () => ffmpegProcess.kill());
         res.on('finish', () => ffmpegProcess.kill());
         res.on('close', () => ffmpegProcess.kill());
         ffmpegProcess.on('error', (err) => {
