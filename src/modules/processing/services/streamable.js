df894864ce9ca8f07940c2e4cabf79903790b243
Blobadoodle <me@bloba.dev>
2023-08-19 18:34:43 +0100

    useless comments

--- src/modules/processing/services/streamable.js
+++ src/modules/processing/services/streamable.js
@@ -8,14 +8,14 @@ export default async function(obj) {
         }).catch(() => { return false });
 
     if (video === undefined) return { error: 'ErrorEmptyDownload' } ;
-    else if (!video) return { error: 'ErrorCouldntFetch' }; // not sure if this is the correct error here, should it be this or ErrorCantConnectToServiceAPI
+    else if (!video) return { error: 'ErrorCouldntFetch' };
 
     let best;
-    if (obj.isAudioOnly || obj.quality === "max" || obj.quality >= "720") // audio seems to be compressed on the mp4-mobile version so isAudiOnly only usese the higest quality
+    if (obj.isAudioOnly || obj.quality === "max" || obj.quality >= "720") // audio seems to be compressed on the mp4-mobile version so isAudioOnly only uses the higest quality
         best = video.files.mp4 ?? video.files['mp4-mobile'];
     else
         best = video.files['mp4-mobile'] ?? video.files.mp4;
 
-    if (best) return { urls: best.url, filename: `streamable_${obj.id}_${best.width}x${best.height}.mp4`, audioFilename: `streamable_${obj.id}_audio` }; // video filename isnt actually used since its redirected but who cares
+    if (best) return { urls: best.url, filename: `streamable_${obj.id}_${best.width}x${best.height}.mp4`, audioFilename: `streamable_${obj.id}_audio` };
     else return { error: 'ErrorEmptyDownload' }
 }
