890b7c8924945fff26f5d7953450a5eebc1f941f
wukko <me@wukko.me>
2023-08-20 15:18:21 +0600

    streamable: clean up + more patterns

--- src/modules/processing/services/streamable.js
+++ src/modules/processing/services/streamable.js
@@ -1,21 +1,19 @@
 export default async function(obj) {
-    const video = await fetch(`https://api.streamable.com/videos/${obj.id}`)
-        .then((r) => {
-            if (r.status === 404)
-                return undefined;
-            else
-                return r.json(); 
-        }).catch(() => { return false });
+    let video = await fetch(`https://api.streamable.com/videos/${obj.id}`).then((r) => { return r.status === 200 ? r.json() : false }).catch(() => { return false });
+    if (!video) return { error: 'ErrorEmptyDownload' };
 
-    if (video === undefined) return { error: 'ErrorEmptyDownload' } ;
-    else if (!video) return { error: 'ErrorCouldntFetch' };
+    let best = video.files['mp4-mobile'];
+    if (video.files.mp4 && (obj.isAudioOnly || obj.quality === "max" || obj.quality >= Number("720"))) {
+        best = video.files.mp4;
+    }
 
-    let best;
-    if (obj.isAudioOnly || obj.quality === "max" || obj.quality >= "720") // audio seems to be compressed on the mp4-mobile version so isAudioOnly only uses the higest quality
-        best = video.files.mp4 ?? video.files['mp4-mobile'];
-    else
-        best = video.files['mp4-mobile'] ?? video.files.mp4;
-
-    if (best) return { urls: best.url, filename: `streamable_${obj.id}_${best.width}x${best.height}.mp4`, audioFilename: `streamable_${obj.id}_audio` };
-    else return { error: 'ErrorEmptyDownload' }
+    if (best) return {
+        urls: best.url,
+        filename: `streamable_${obj.id}_${best.width}x${best.height}.mp4`,
+        audioFilename: `streamable_${obj.id}_audio`,
+        metadata: {
+            title: video.title
+        }
+    }
+    return { error: 'ErrorEmptyDownload' }
 }
