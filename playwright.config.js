9497ffc05dd84192db8131614f2b45fa8ec6060a
ndom91 <yo@ndo.dev>
2023-12-19 13:49:47 +0100

    fix: always retain trace on failure

--- playwright.config.js
+++ playwright.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
     baseURL: 'http://127.0.0.1:9001',
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
   projects: [
     {
