b3e07e236b2ef548f2f3f7b4ad2f40c32afac6c6
wukko <me@wukko.me>
2024-02-13 13:50:06 +0600

    loadFromFs: fix shadowing variable name

--- api/modules/util/loadFromFs.js
+++ api/modules/util/loadFromFs.js
@@ -7,16 +7,9 @@ const splitDir = path.dirname(fileURLToPath(import.meta.url)).split('/');
 splitDir.splice(-2);
 const dir = splitDir.join('/');
 
-export function loadJSON(path) {
+export function loadJSON(filePath) {
     try {
-        return JSON.parse(fs.readFileSync(`${dir}/${path}`, 'utf-8'))
-    } catch(e) {
-        return false
-    }
-}
-export function loadFile(path) {
-    try {
-        return fs.readFileSync(path, 'utf-8')
+        return JSON.parse(fs.readFileSync(`${dir}/${filePath}`, 'utf-8'))
     } catch(e) {
         return false
     }
