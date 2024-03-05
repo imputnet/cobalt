fc39ac76b6471da4815efcb316bb59c5e28d18a8
wukko <me@wukko.me>
2024-03-05 21:18:20 +0600

    crypto: change input var name in stream functions

--- src/modules/sub/crypto.js
+++ src/modules/sub/crypto.js
@@ -7,8 +7,8 @@ export function generateHmac(str, salt) {
     return createHmac("sha256", salt).update(str).digest("base64url");
 }
 
-export function encryptStream(str, iv, secret) {
-    const buff = Buffer.from(JSON.stringify(str), "utf-8");
+export function encryptStream(plaintext, iv, secret) {
+    const buff = Buffer.from(JSON.stringify(plaintext), "utf-8");
 
     const key = scryptSync(Buffer.from(secret, "base64url"), "salt", keyLength);
     const cipher = createCipheriv(algorithm, key, Buffer.from(iv, "base64url"));
@@ -16,8 +16,8 @@ export function encryptStream(str, iv, secret) {
     return Buffer.concat([ cipher.update(buff), cipher.final() ])
 }
 
-export function decryptStream(buf, iv, secret) {
-    const buff = Buffer.from(buf, "binary");
+export function decryptStream(ciphertext, iv, secret) {
+    const buff = Buffer.from(ciphertext, "binary");
 
     const key = scryptSync(Buffer.from(secret, "base64url"), "salt", keyLength);
     const decipher = createDecipheriv(algorithm, key, Buffer.from(iv, "base64url"));
