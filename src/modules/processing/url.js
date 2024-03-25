68d8fa1515fab4d68126828737ad11e35cc321f5
hyperdefined <contact@hyper.lol>
2024-03-25 13:32:28 -0400

    feat: ddinstagram.com support

--- src/modules/processing/url.js
+++ src/modules/processing/url.js
@@ -64,6 +64,11 @@ export function aliasURL(url) {
             if (url.hostname === 'dai.ly' && parts.length === 2) {
                 url = new URL(`https://dailymotion.com/video/${parts[1]}`)
             }
+        case "ddinstagram":
+            if (services.instagram.altDomains.includes(url.hostname)) {
+                url.hostname = 'instagram.com'
+            }
+            break;
     }
 
     return url
