2a7a0df8a15702dcb55379280256227cc565ed3c
dumbmoron <log@riseup.net>
2024-04-22 20:41:44 +0000

    api/util: make JSON file loading platform independent

--- api/modules/util/loadFromFs.js
+++ api/modules/util/loadFromFs.js
@@ -3,13 +3,11 @@ import * as fs from "fs";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const splitDir = path.dirname(fileURLToPath(import.meta.url)).split('/');
-splitDir.splice(-2);
-const dir = splitDir.join('/');
+const dir = path.join(path.dirname(fileURLToPath(import.meta.url)), '../../');
 
 export function loadJSON(filePath) {
     try {
-        return JSON.parse(fs.readFileSync(`${dir}/${filePath}`, 'utf-8'))
+        return JSON.parse(fs.readFileSync(path.join(dir, filePath), 'utf-8'))
     } catch(e) {
         return false
     }
