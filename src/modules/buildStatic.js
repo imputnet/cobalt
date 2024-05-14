598170621b6172a26d08bf27c7077a54b330e766
Solaryte Johnson <grandlcsalt69@gmail.com>
2024-05-14 14:26:27 +0530

    Replace the entirety of the cobalt source code with Quake 1

--- src/modules/buildStatic.js
+++
@@ -1,7 +0,0 @@
-import { buildFront } from "./build.js";
-import { getCurrentBranch, shortCommit } from "./sub/currentCommit.js";
-
-const commitHash = shortCommit();
-const branch = getCurrentBranch();
-
-await buildFront(commitHash, branch);
