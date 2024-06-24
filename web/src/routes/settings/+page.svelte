530edee0b18445eed13334091d0973e72e3060e9
wukko <me@wukko.me>
2024-06-24 19:44:06 +0600

    web/settings: update main page placeholder

--- web/src/routes/settings/+page.svelte
+++ web/src/routes/settings/+page.svelte
@@ -1,13 +1,1 @@
-<script lang="ts"> 
-    import SettingsButton from "../../components/buttons/SettingsButton.svelte";
-    import SettingsCheckbox from "../../components/buttons/SettingsCheckbox.svelte";
-    import Switcher from "../../components/buttons/Switcher.svelte";
-
-    import { merge } from 'ts-deepmerge';
-    import defaultSettings, { settingArrays } from "$lib/settings/defaults";
-
-    const switchers = merge(defaultSettings, settingArrays);
-</script>
-
-<div>
-</div>
+<div>settings home page (should not be visible)</div>
