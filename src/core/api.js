ea69fd8ded0cffa65fa58b27fd2c48724c72d90b
alina sireneva <alina@tei.su>
2024-06-25 15:25:00 +0300

    feat: support external proxies

--- src/core/api.js
+++ src/core/api.js
@@ -1,5 +1,6 @@
 import cors from "cors";
 import rateLimit from "express-rate-limit";
+import { setGlobalDispatcher, ProxyAgent } from "undici";
 
 import { env, version } from "../modules/config.js";
 
@@ -218,6 +219,10 @@ export function runAPI(express, app, gitCommit, gitBranch, __dirname) {
 
     randomizeCiphers();
     setInterval(randomizeCiphers, 1000 * 60 * 30); // shuffle ciphers every 30 minutes
+
+    if (env.externalProxy) {
+        setGlobalDispatcher(new ProxyAgent(env.externalProxy))
+    }
 
     app.listen(env.apiPort, env.listenAddress, () => {
         console.log(`\n` +
