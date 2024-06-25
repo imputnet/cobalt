635561394cb3cd2e72cc41ec78da3a723a407064
wukko <me@wukko.me>
2024-06-25 21:01:08 +0600

    web: add dynamic page titles

--- web/src/routes/donate/+page.svelte
+++ web/src/routes/donate/+page.svelte
@@ -2,4 +2,10 @@
     import Placeholder from "$components/misc/Placeholder.svelte";
 </script>
 
+<svelte:head>
+    <title>
+        cobalt: donate
+    </title>
+</svelte:head>
+
 <Placeholder pageName="donate" />
--- web/src/routes/updates/+page.svelte
+++ web/src/routes/updates/+page.svelte
@@ -2,4 +2,10 @@
     import Placeholder from "$components/misc/Placeholder.svelte";
 </script>
 
+<svelte:head>
+    <title>
+        cobalt: updates
+    </title>
+</svelte:head>
+
 <Placeholder pageName="updates" />
