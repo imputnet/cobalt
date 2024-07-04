6651082d589073808a4e747981dc101679c8c8f7
dumbmoron <log@riseup.net>
2024-07-04 22:18:02 +0000

    web/settings: redirect invalid settings paths to default settings page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { defaultSettingsPage } from '$lib/settings/defaults';
+
+	$: {
+		if (
+			$page.error?.message === 'Not Found'
+			&& $page.url.pathname.startsWith('/settings')
+		) {
+			goto(defaultSettingsPage(), { replaceState: true });
+		}
+	}
+</script>
+
+<!-- TODO: nicer errors -->
+<h1>{$page.status}: {$page.error?.message}</h1>
