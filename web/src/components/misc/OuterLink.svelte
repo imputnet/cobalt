8862abb88e541f77f26533e0a7a8f7a90e97b3c6
wukko <me@wukko.me>
2024-07-14 21:51:56 +0600

    web/settings: update analytics text & add a link to more info

---
+++ web/src/components/misc/OuterLink.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let href: string;
+</script>
+
+<a target="_blank" rel="noopener noreferrer" {href}>
+    <slot></slot>
+</a>
--- web/src/routes/settings/general/privacy/+page.svelte
+++ web/src/routes/settings/general/privacy/+page.svelte
@@ -3,13 +3,25 @@
 
     import SettingsCategory from "$components/settings/SettingsCategory.svelte";
     import SettingsToggle from "$components/buttons/SettingsToggle.svelte";
+    import OuterLink from "$components/misc/OuterLink.svelte";
 </script>
 
-<SettingsCategory sectionId="privacy" title={$t("settings.privacy")}>
+<SettingsCategory sectionId="privacy" title={$t("settings.privacy.analytics")}>
     <SettingsToggle
         settingContext="privacy"
         settingId="disableAnalytics"
-        title={$t("settings.privacy.disableAnalytics.title")}
-        description={$t("settings.privacy.disableAnalytics.description")}
+        title={$t("settings.privacy.analytics.title")}
+        description={$t("settings.privacy.analytics.description")}
     />
+    <div class="subtext learn-more-plausible">
+        <OuterLink href="https://plausible.io/privacy-focused-web-analytics">
+            {$t("settings.privacy.analytics.learnmore")}
+        </OuterLink>
+    </div>
 </SettingsCategory>
+
+<style>
+    .learn-more-plausible {
+        padding-top: 6px;
+    }
+</style>
