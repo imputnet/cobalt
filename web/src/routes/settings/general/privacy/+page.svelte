4168998b93bab5deaeab0c17c9d784688964dfe9
dumbmoron <log@riseup.net>
2024-07-14 15:26:55 +0000

    web/settings: add "privacy" page with analytics toggle

---
+++ web/src/routes/settings/general/privacy/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { t } from "$lib/i18n/translations";
+
+    import SettingsCategory from "$components/settings/SettingsCategory.svelte";
+    import SettingsToggle from "$components/buttons/SettingsToggle.svelte";
+</script>
+
+<SettingsCategory sectionId="privacy" title={$t("settings.privacy")}>
+    <SettingsToggle
+        settingContext="privacy"
+        settingId="disableAnalytics"
+        title={$t("settings.privacy.disableAnalytics.title")}
+        description={$t("settings.privacy.disableAnalytics.description")}
+    />
+</SettingsCategory>
