6e374fde622988b5f7927f94cc00703786d24da1
wukko <me@wukko.me>
2024-07-17 14:04:53 +0600

    web/updates: make changelogs look nicer
    - fixes horizontal scrolling on mobile
    - removes text backdrop
    - improves readability

--- web/src/components/misc/ChangelogEntryWrapper.svelte
+++ web/src/components/misc/ChangelogEntryWrapper.svelte
@@ -1,9 +1,9 @@
 <!-- Workaround for https://github.com/pngwn/MDsveX/issues/116 -->
 <script lang="ts">
     import ChangelogEntry from "./ChangelogEntry.svelte";
-    export let version = '';
-    export let title = '';
-    export let date = '';
+    export let version = "";
+    export let title = "";
+    export let date = "";
     export let banner = undefined;
 </script>
 
