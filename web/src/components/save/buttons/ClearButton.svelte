ee162aa23635cf4ffef091f53ff7ee5f9923e0fa
wukko <me@wukko.me>
2024-07-23 12:13:40 +0600

    web/ClearButton: fix rendering bug in safari & clean up

--- web/src/components/save/buttons/ClearButton.svelte
+++ web/src/components/save/buttons/ClearButton.svelte
@@ -1,17 +1,29 @@
 <script>
     import { t } from "$lib/i18n/translations";
-    import IconX from '@tabler/icons-svelte/IconX.svelte';
+    import IconX from "@tabler/icons-svelte/IconX.svelte";
 
     export let click;
 </script>
 
-<button id="clear-button" on:click={click} aria-label={$t("a11y.save.clearInput")}>
-    <IconX color="var(--secondary)" size="16px"/>
+<button
+    id="clear-button"
+    on:click={click}
+    aria-label={$t("a11y.save.clearInput")}
+>
+    <IconX />
 </button>
 
 <style>
     #clear-button {
         padding: 3px;
         border-radius: 100%;
+    }
+
+    #clear-button :global(svg) {
+        width: 16px;
+        height: 16px;
+        stroke-width: 2px;
+        stroke: var(--secondary);
+        will-change: transform;
     }
 </style>
