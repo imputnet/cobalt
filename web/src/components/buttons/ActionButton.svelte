26eaac5742ce00971a232b230edf4684cbfc97e9
wukko <me@wukko.me>
2024-07-27 15:07:38 +0600

    web/ActionButton: clean up

--- web/src/components/buttons/ActionButton.svelte
+++ web/src/components/buttons/ActionButton.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     export let id: string;
-    export let click = () => { alert('no function assigned') };
+    export let click = () => {
+        alert("no function assigned");
+    };
 </script>
 
 <button id="button-{id}" class="button" on:click={click}>
