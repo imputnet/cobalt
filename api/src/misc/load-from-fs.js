0e0ad7cb0e8743163e217470c162f8059b506fe3
dumbmoron <log@riseup.net>
2024-08-03 09:27:56 +0000

    api/load-from-fs: always load files from root of api folder

--- api/src/misc/load-from-fs.js
+++ api/src/misc/load-from-fs.js
@@ -1,12 +1,20 @@
 import * as fs from "fs";
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const root = join(
+    dirname(fileURLToPath(import.meta.url)),
+    '../../'
+);
 
 export function loadJSON(path) {
     try {
-        return JSON.parse(fs.readFileSync(path, 'utf-8'))
+        return JSON.parse(fs.readFileSync(join(root, path), 'utf-8'))
     } catch {
         return false
     }
 }
+
 export function loadFile(path) {
     try {
         return fs.readFileSync(path, 'utf-8')
