75ef4604d87724bc369e06a6fa1ef72c0e8ae3e5
dumbmoron <log@riseup.net>
2024-08-12 16:47:11 +0000

    web/vite: expose libav.js files directly

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,22 +1,72 @@
+import { defineConfig, searchForWorkspaceRoot, type PluginOption } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig, searchForWorkspaceRoot } from "vite";
-
 import basicSSL from "@vitejs/plugin-basic-ssl";
+import { glob } from "glob";
+import mime from "mime";
+
+import { cp, readdir, mkdir } from "node:fs/promises";
+import { createReadStream } from "node:fs";
+import { join, basename } from "node:path";
+
+const exposeLibAV: PluginOption = (() => {
+    const IMPUT_MODULE_DIR = join(__dirname, 'node_modules/@imput');
+    return {
+        name: "vite-libav.js",
+        configureServer(server) {
+            server.middlewares.use(async (req, res, next) => {
+                if (!req.url?.startsWith('/_libav/')) return next();
+
+                const filename = basename(req.url).split('?')[0];
+                if (!filename) return next();
+
+                const [ file ] = await glob(join(IMPUT_MODULE_DIR, '/**/dist/', filename));
+                if (!file) return next();
+
+                const fileType = mime.getType(filename);
+                if (!fileType) return next();
+
+                res.setHeader('Content-Type', fileType);
+                return createReadStream(file).pipe(res);
+            });
+        },
+        generateBundle: async (options) => {
+            if (!options.dir) {
+                return;
+            }
+
+            const assets = join(options.dir, '_libav');
+            await mkdir(assets, { recursive: true });
+
+            const modules = await readdir(IMPUT_MODULE_DIR).then(
+                modules => modules.filter(m => m.startsWith('libav.js'))
+            );
+
+            for (const module of modules) {
+                const distFolder = join(IMPUT_MODULE_DIR, module, 'dist/');
+                console.log(distFolder);
+                await cp(distFolder, assets, { recursive: true });
+            }
+        }
+    }
+})();
+
+const enableCOEP: PluginOption = {
+    name: "isolation",
+    configureServer(server) {
+        server.middlewares.use((_req, res, next) => {
+            res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+            res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+            next();
+        })
+    }
+};
 
 export default defineConfig({
     plugins: [
         basicSSL(),
         sveltekit(),
-        {
-            name: "isolation",
-            configureServer(server) {
-                server.middlewares.use((_req, res, next) => {
-                    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-                    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-                    next();
-                })
-            }
-        }
+        enableCOEP,
+        exposeLibAV
     ],
     build: {
         rollupOptions: {
@@ -33,6 +83,10 @@ export default defineConfig({
         }
     },
     server: {
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "require-corp"
+        },
         fs: {
             allow: [
                 searchForWorkspaceRoot(process.cwd())
@@ -41,6 +95,6 @@ export default defineConfig({
         proxy: {}
     },
     optimizeDeps: {
-        exclude: ["@imput/ffmpeg.wasm"]
+        exclude: [ "@imput/libav.js-remux-cli" ]
     },
 });
