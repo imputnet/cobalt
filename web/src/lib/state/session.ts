3336210e93c34114697df87278409d7aefbfdfc7
wukko <me@wukko.me>
2024-08-16 23:48:50 +0600

    web/state/session: clean up

--- web/src/lib/state/session.ts
+++ web/src/lib/state/session.ts
@@ -1,6 +1,4 @@
 import { writable } from "svelte/store";
-
-import type { Writable } from "svelte/store";
 import type { CobaltSession } from "$lib/types/api";
 
-export const cachedSession: Writable<CobaltSession | null> = writable();
+export const cachedSession = writable<CobaltSession | undefined>();
