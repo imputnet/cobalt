5dc0cf1772268bf360b21f5fc0cca50f4b0facec
wukko <me@wukko.me>
2024-08-29 13:20:17 +0600

    web/processing: hide override section when `DEFAULT_API` isn't present

--- web/src/routes/settings/processing/+page.svelte
+++ web/src/routes/settings/processing/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+    import env from "$lib/env";
     import { t } from "$lib/i18n/translations";
 
-    import SettingsCategory from "$components/settings/SettingsCategory.svelte";
     import SettingsToggle from "$components/buttons/SettingsToggle.svelte";
+    import SettingsCategory from "$components/settings/SettingsCategory.svelte";
 </script>
 
-<SettingsCategory sectionId="override" title={$t("settings.processing.override")}>
-    <SettingsToggle
-        settingContext="processing"
-        settingId="allowDefaultOverride"
-        title={$t("settings.processing.override.title")}
-        description={$t("settings.processing.override.description")}
-    />
-</SettingsCategory>
+{#if env.DEFAULT_API}
+    <SettingsCategory sectionId="override" title={$t("settings.processing.override")}>
+        <SettingsToggle
+            settingContext="processing"
+            settingId="allowDefaultOverride"
+            title={$t("settings.processing.override.title")}
+            description={$t("settings.processing.override.description")}
+        />
+    </SettingsCategory>
+{/if}
