b7e5a9422601865054d325abb8ea6e06c84b7756
dumbmoron <log@riseup.net>
2024-09-03 13:38:07 +0000

    web/svelte: enable fallback page generation

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -25,7 +25,7 @@ const config = {
             // these options are set automatically — see below
             pages: 'build',
             assets: 'build',
-            fallback: undefined,
+            fallback: '404.html',
             precompress: false,
             strict: true
         }),
