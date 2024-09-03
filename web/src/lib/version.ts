1262bc20fe6c7fcb04606e15908cffb845edec83
dumbmoron <log@riseup.net>
2024-09-03 13:33:16 +0000

    web/version: don't try to fetch when server-side rendering

--- web/src/lib/version.ts
+++ web/src/lib/version.ts
@@ -1,5 +1,6 @@
 import { readable } from "svelte/store";
 import type { Optional } from "./types/generic";
+import { browser } from "$app/environment";
 
 type VersionResponse = {
     commit: string;
@@ -11,6 +12,8 @@ type VersionResponse = {
 export const version = readable<Optional<VersionResponse>>(
     undefined,
     (set) => {
+        if (!browser) return;
+
         fetch('/version.json')
             .then(r => r.json())
             .then(set)
