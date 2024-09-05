b1b8f6967bc4c3028f5e2cc1214a2f500982c26e
wukko <me@wukko.me>
2024-09-05 10:22:02 +0600

    web/about/terms: initial terms and ethics page

--- web/src/routes/about/terms/+page.svelte
+++ web/src/routes/about/terms/+page.svelte
@@ -1,1 +1,34 @@
-<div>terms and ethics here</div>
+<div id="terms-body" class="long-text-noto">
+    <section id="saving">
+        <h3>saving</h3>
+        <p>
+            saving functionality simplifies downloading content from the internet and takes zero liability for what the saved content is used for. processing servers work like advanced proxies and don't ever write any content to disk. everything is handled in RAM and permanently purged once the tunnel is done. we have no downloading logs and can't identify anyone.
+        </p>
+        <p>
+            <a href="/about/privacy">you can read more about how tunnels work in our privacy policy.</a>
+        </p>
+    </section>
+
+    <section id="responsibiliy">
+        <h3>responsibilities</h3>
+        <p>
+            you (end user) are responsible for what you do with our tools, how you use and distribute resulting content. please be mindful when using content of others and always credit original creators. make sure you don't violate any terms or licenses.
+        </p>
+        <p>
+            when used in educational purposes, always cite sources and credit original creators.
+        </p>
+        <p>
+            fair use and credits benefit everyone.
+        </p>
+    </section>
+
+    <section id="other-responsibiliy">
+        <h3>other responsibilities</h3>
+        <p>
+            when our tools are used for potentially dangerous activities, we have no way of detecting them, as cobalt is 100% anonymous.
+        </p>
+        <p>
+            however, you can report such activities to us manually and we will do our best to comply: <a href="mailto:safety@imput.net">safety@imput.net</a>
+        </p>
+    </section>
+</div>
