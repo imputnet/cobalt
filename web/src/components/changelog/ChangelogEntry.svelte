2fcda9c7056dd0f57c03d8731a70ded4f1deda3f
dumbmoron <log@riseup.net>
2024-09-09 20:35:12 +0000

    web/ChangelogEntry: fix off-by-one changelog date in some cases

--- web/src/components/changelog/ChangelogEntry.svelte
+++ web/src/components/changelog/ChangelogEntry.svelte
@@ -15,15 +15,14 @@
 
     const formatDate = (dateString: string) => {
         const date = new Date(dateString);
-
         const months = ["January", "February", "March", "April", "May",
                         "June", "July", "August", "September", "October",
                         "November", "December"];
 
         return [
-            months[date.getUTCMonth()],
-            date.getUTCDate() + 1 + ",",
-            date.getUTCFullYear(),
+            months[date.getMonth()],
+            date.getDate() + ",",
+            date.getFullYear(),
         ].join(" ");
     };
 
