366b77ad3e57c04895dfdbf5b2a69eb5c57b87b1
Tanner Sommers <tanner@teamhydra.dev>
2024-09-17 12:22:01 -0500

    tests: test on firefox and webkit instead of just chrome

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -21,15 +21,15 @@ export default defineConfig({
             use: { ...devices['Desktop Chrome'] },
         },
 
-        // {
-        //     name: 'firefox',
-        //     use: { ...devices['Desktop Firefox'] },
-        // },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
 
-        // {
-        //     name: 'webkit',
-        //     use: { ...devices['Desktop Safari'] },
-        // },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
 
         // Test on mobile devices (might be useful in the future)?
         // {
