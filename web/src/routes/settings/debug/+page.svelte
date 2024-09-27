39c269d70ccae306fbd66d6833b7ac1f4120a1b4
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2024-09-27 12:36:35 -0700

    debug/+page.svelte: use translations, add copy button, iterate through array for template

--- web/src/routes/settings/debug/+page.svelte
+++ web/src/routes/settings/debug/+page.svelte
@@ -2,10 +2,34 @@
     import { onMount } from "svelte";
     import { goto } from "$app/navigation";
 
+    import ActionButton from "$components/buttons/ActionButton.svelte";
+    import CopyIcon from "$components/misc/CopyIcon.svelte";
+
+    import { t } from "$lib/i18n/translations";
+    import { copyURL } from "$lib/download";
     import { version } from "$lib/version";
     import { device, app } from "$lib/device";
     import { defaultNavPage } from "$lib/subnav";
     import settings, { storedSettings } from "$lib/state/settings";
+
+    $: sections = [
+        { title: $t("settings.advanced.debug.device"), data: device },
+        { title: $t("settings.advanced.debug.app"), data: app },
+        {
+            title: $t("settings.advanced.debug.settings"),
+            data: $storedSettings,
+        },
+        { title: $t("settings.advanced.debug.version"), data: $version },
+    ];
+
+    let lastCopiedSection = -1;
+    let lastCopiedSectionResetTimeout: ReturnType<typeof setTimeout>;
+
+    $: if (lastCopiedSection) {
+        lastCopiedSectionResetTimeout = setTimeout(() => {
+            lastCopiedSection = -1;
+        }, 1500);
+    }
 
     onMount(() => {
         if (!$settings.advanced.debug) {
@@ -16,25 +40,25 @@
 
 {#if $settings.advanced.debug}
     <div id="advanced-page">
-        <h3>device:</h3>
-        <div class="message-container subtext">
-            {JSON.stringify(device, null, 2)}
-        </div>
-
-        <h3>app:</h3>
-        <div class="message-container subtext">
-            {JSON.stringify(app, null, 2)}
-        </div>
-
-        <h3>settings:</h3>
-        <div class="message-container subtext">
-            {JSON.stringify($storedSettings, null, 2)}
-        </div>
-
-        <h3>version:</h3>
-        <div class="message-container subtext">
-            {JSON.stringify($version, null, 2)}
-        </div>
+        {#each sections as { title, data }, i}
+            <h3>{title}:</h3>
+            <div class="message-container subtext">
+                {JSON.stringify(data, null, 2)}
+            </div>
+            <ActionButton
+                id="copy-button"
+                click={() => {
+                    clearTimeout(lastCopiedSectionResetTimeout);
+                    lastCopiedSection = i;
+                    copyURL(JSON.stringify(data, null, 2));
+                }}
+            >
+                <CopyIcon />
+                {lastCopiedSection === i
+                    ? $t("button.copied")
+                    : $t("button.copy")}
+            </ActionButton>
+        {/each}
     </div>
 {/if}
 
