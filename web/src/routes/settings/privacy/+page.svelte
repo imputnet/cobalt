ce131b1454aedee2488cdc0f4e5f4b5d637924e5
wukko <me@wukko.me>
2024-10-28 23:18:04 +0600

    web/settings/privacy: remove beta tag from tunneling

--- web/src/routes/settings/privacy/+page.svelte
+++ web/src/routes/settings/privacy/+page.svelte
@@ -7,7 +7,7 @@
     import SettingsCategory from "$components/settings/SettingsCategory.svelte";
 </script>
 
-<SettingsCategory sectionId="tunnel" title={$t("settings.privacy.tunnel")} beta>
+<SettingsCategory sectionId="tunnel" title={$t("settings.privacy.tunnel")}>
     <SettingsToggle
         settingContext="privacy"
         settingId="alwaysProxy"
