778ee76d59799f6b8c36ed35a5b110f1b7d04acc
wukko <me@wukko.me>
2024-11-18 16:42:59 +0600

    web/Omnibox: fix main instance domain check
    oops

--- web/src/components/save/Omnibox.svelte
+++ web/src/components/save/Omnibox.svelte
@@ -139,7 +139,7 @@
     if you want to remove the community instance label,
     refer to the license first https://github.com/imputnet/cobalt/tree/main/web#license
 -->
-{#if env.DEFAULT_API || $page.url.host !== "cobalt.tools" || !$page.url.host.endsWith(".cobalt.tools")}
+{#if env.DEFAULT_API || (!$page.url.host.endsWith(".cobalt.tools") && $page.url.host !== "cobalt.tools")}
     <div id="instance-label">
         {$t("save.label.community_instance")}
     </div>
