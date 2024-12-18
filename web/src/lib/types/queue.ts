13ec4f4faffdbe5e904791749a91fda0d2237917
wukko <me@wukko.me>
2024-12-18 16:59:08 +0600

    web/queue: add types & states

--- web/src/lib/types/queue.ts
+++ web/src/lib/types/queue.ts
@@ -1,7 +1,34 @@
+type ProcessingStep = "mux" | "mux_hls" | "encode";
+type ProcessingPreset = "mp4" | "webm" | "copy";
+type ProcessingState = "completed" | "failed" | "canceled" | "waiting" | "downloading" | "muxing" | "converting";
+type ProcessingType = "video" | "video_mute" | "audio" | "audio_convert" | "image" | "gif";
+type QueueFileType = "video" | "audio" | "image" | "gif";
+
+export type ProcessingStepItem = {
+    type: ProcessingStep,
+    preset?: ProcessingPreset,
+}
+
+export type QueueFile = {
+    type: QueueFileType,
+    url: string,
+}
+
 export type QueueItem = {
     id: string,
-    type: "video" | "audio" | "mute" | "image" | "gif",
+    status: ProcessingState,
+    type: ProcessingType,
     filename: string,
-    status: string,
-    progress: number,
+    files: QueueFile[],
+    processingSteps: ProcessingStepItem[],
+}
+
+export type OngoingQueueItem = {
+    id: string,
+    currentStep?: ProcessingStep,
+    size?: {
+        expected: number,
+        current: number,
+    },
+    speed?: number,
 }
