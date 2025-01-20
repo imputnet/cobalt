de5eca19a5c3d82462b691b8831034985c98a847
wukko <me@wukko.me>
2025-01-20 19:30:11 +0600

    api/utils: replace redirectStatuses array with a set
    Co-authored-by: jj <log@riseup.net>

--- api/src/misc/utils.js
+++ api/src/misc/utils.js
@@ -1,11 +1,11 @@
-const redirectStatuses = [301, 302, 303, 307, 308];
+const redirectStatuses = new Set([301, 302, 303, 307, 308]);
 
 export async function getRedirectingURL(url, dispatcher) {
     const location = await fetch(url, {
         redirect: 'manual',
         dispatcher,
     }).then((r) => {
-        if (redirectStatuses.includes(r.status) && r.headers.has('location')) {
+        if (redirectStatuses.has(r.status) && r.headers.has('location')) {
             return r.headers.get('location');
         }
     }).catch(() => null);
