f5df78ffec4c4b5b37aa73a302bd0535719032f7
jj <log@riseup.net>
2025-04-02 12:29:18 +0000

    api/utils: retry getting redirecting url with fetch() if request() fails

--- api/src/misc/utils.js
+++ api/src/misc/utils.js
@@ -2,13 +2,22 @@ import { request } from 'undici';
 const redirectStatuses = new Set([301, 302, 303, 307, 308]);
 
 export async function getRedirectingURL(url, dispatcher, headers) {
-    const location = await request(url, {
+    const params = {
         dispatcher,
         method: 'HEAD',
-        headers: headers
-    }).then(r => {
+        headers,
+        redirect: 'manual'
+    };
+
+    let location = await request(url, params).then(r => {
         if (redirectStatuses.has(r.statusCode) && r.headers['location']) {
             return r.headers['location'];
+        }
+    }).catch(() => null);
+
+    location ??= await fetch(url, params).then(r => {
+        if (redirectStatuses.has(r.status) && r.headers.has('location')) {
+            return r.headers.get('location');
         }
     }).catch(() => null);
 
