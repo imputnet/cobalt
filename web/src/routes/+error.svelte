b304549a8d7f415042324d2523b1ed09c6937b6c
wukko <me@wukko.me>
2025-06-03 11:18:42 +0600

    web/routes: refactor error & /about/[page] to svelte 5

--- web/src/routes/+error.svelte
+++ web/src/routes/+error.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { page } from "$app/stores";
+    import { page } from "$app/state";
     import { goto } from "$app/navigation";
     import { defaultNavPage } from "$lib/subnav";
 
     onMount(() => {
-        if ($page.error?.message === "Not Found") {
-            if ($page.url.pathname.startsWith("/settings")) {
+        if (page.error?.message === "Not Found") {
+            if (page.url.pathname.startsWith("/settings")) {
                 goto(defaultNavPage("settings"), { replaceState: true });
-            } else if ($page.url.pathname.startsWith("/about")) {
+            } else if (page.url.pathname.startsWith("/about")) {
                 goto(defaultNavPage("about"), { replaceState: true });
             } else {
                 goto("/", { replaceState: true });
