e2b6879ea2c8be11189bc422a47a221a735478bd
jj <log@riseup.net>
2025-06-07 08:34:31 +0000

    api/env: log information about dynamic env changes

--- api/src/config.js
+++ api/src/config.js
@@ -12,12 +12,19 @@ export const canonicalEnv = Object.freeze(structuredClone(process.env));
 export const setTunnelPort = (port) => env.tunnelPort = port;
 export const isCluster = env.instanceCount > 1;
 export const updateEnv = (newEnv) => {
+    const changes = [];
+
     // tunnelPort is special and needs to get carried over here
     newEnv.tunnelPort = env.tunnelPort;
 
     for (const key in env) {
+        if (String(env[key]) !== String(newEnv[key])) {
+            changes.push(key);
+        }
         env[key] = newEnv[key];
     }
+
+    return changes;
 }
 
 await validateEnvs(env);
