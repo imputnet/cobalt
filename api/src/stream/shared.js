358fff6f7ac0784df19c77fedc53d2898c896c57
celebrateyang <celebrateyang@hotmail.com>
2025-06-09 11:14:33 +0800

    prevent youtube detect as bot

--- api/src/stream/shared.js
+++ api/src/stream/shared.js
@@ -10,12 +10,21 @@ const defaultHeaders = {
 const serviceHeaders = {
     bilibili: {
         referer: 'https://www.bilibili.com/'
-    },
-    youtube: {
-        accept: '*/*',
-        origin: 'https://www.youtube.com',
-        referer: 'https://www.youtube.com',
-        DNT: '?1'
+    },    youtube: {
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-US,en;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        referer: 'https://www.youtube.com/',
+        origin: 'https://www.youtube.com'
     },
     vk: {
         'user-agent': vkClientAgent
