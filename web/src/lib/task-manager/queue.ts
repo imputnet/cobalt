17ab8dd709d2fdabe0ab3df6881d73f25addef9e
wukko <me@wukko.me>
2025-06-20 18:30:39 +0600

    web/queue: add subtitles independently from remux type
    so that you can have a mute video with subtitles

--- web/src/lib/task-manager/queue.ts
+++ web/src/lib/task-manager/queue.ts
@@ -68,15 +68,15 @@ const makeRemuxArgs = (info: CobaltLocalProcessingResponse) => {
 
     if (["merge", "remux"].includes(info.type)) {
         ffargs.push("-c:a", "copy");
-
-        if (info.output.subtitles) {
-            ffargs.push(
-                "-c:s",
-                info.output.filename.endsWith(".mp4") ? "mov_text" : "webvtt"
-            );
-        }
     } else if (info.type === "mute") {
         ffargs.push("-an");
+    }
+
+    if (info.output.subtitles) {
+        ffargs.push(
+            "-c:s",
+            info.output.filename.endsWith(".mp4") ? "mov_text" : "webvtt"
+        );
     }
 
     ffargs.push(
