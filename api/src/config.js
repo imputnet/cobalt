40da8a46d609bf8f7e33ff0279ae825d7be0059b
wukko <me@wukko.me>
2025-07-07 20:09:02 +0600

    api/config: update chromium version in generic user agent

--- api/src/config.js
+++ api/src/config.js
@@ -5,7 +5,7 @@ const version = await getVersion();
 
 const env = loadEnvs();
 
-const genericUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
+const genericUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36";
 const cobaltUserAgent = `cobalt/${version} (+https://github.com/imputnet/cobalt)`;
 
 export const canonicalEnv = Object.freeze(structuredClone(process.env));
