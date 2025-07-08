51a9680b39cc353bdcd0fa6a52f9eed920b58736
wukko <me@wukko.me>
2025-07-08 20:58:19 +0600

    api/match: fix localDisabled
    accidentally left the old naming of the option here, typescript would've prevented this

--- api/src/processing/match.js
+++ api/src/processing/match.js
@@ -314,7 +314,7 @@ export default async function({ host, patternMatch, params, authType }) {
         let localProcessing = params.localProcessing;
         const lpEnv = env.forceLocalProcessing;
         const shouldForceLocal = lpEnv === "always" || (lpEnv === "session" && authType === "session");
-        const localDisabled = (!localProcessing || localProcessing === "none");
+        const localDisabled = (!localProcessing || localProcessing === "disabled");
 
         if (shouldForceLocal && localDisabled) {
             localProcessing = "preferred";
